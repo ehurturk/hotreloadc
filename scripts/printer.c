e0595d6f94e3bf34c8cf917bf0df50642c687d73
Emir Hurturk <emirhurturk444@gmail.com>
2021-11-24 23:28:02 +0300

    added another script: printer.c

---
+++ scripts/printer.c
@@ -0,0 +1,38 @@
+#include "../core/core.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct printer {
+  int ch;
+};
+
+typedef struct printer printer_t;
+
+printer_t *m_instance;
+
+size_t get_instance_size() {
+  printf("hello\n");
+  return sizeof(printer_t);
+}
+
+/*
+ * would be called once the plugin is modified+compiled runtime
+ * it is advised to initialize necessary variables in here (such as malloc'ing
+ * pointers)
+ */
+void on_plugin_reload(void *ptr) {
+  printf("reload\n");
+  m_instance = (printer_t *)ptr;
+}
+
+/* would be called in the update loop */
+void on_plugin_update() {
+  // int ch = get_async_key();
+  // if (ch == 65) {
+  //   char *msg = NULL;
+  //   sprintf(msg, "Player velocity: %i\n", m_instance->velocity);
+  //   print_window(msg);
+  // }
+  printf("Printer is printing!\n");
+}
